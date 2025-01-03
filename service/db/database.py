c1bcc4595c3c13451bc4567eea292b0f669c4e1e
mogech <al.batow@yandex.rucat>
2025-01-03 23:18:21 +0300

    corrected

--- service/db/database.py
+++ service/db/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from settings import settings
+# from settings import settings
+from dotenv import load_dotenv
+import os
 # Настройте строку подключения к базе данных
-DATABASE_URL = settings.postgres_dsn  
+load_dotenv()
+DATABASE_URL = str(os.getenv("DB_URL",""))  
 
 # Создаём движок базы данных
 engine = create_engine(DATABASE_URL)
